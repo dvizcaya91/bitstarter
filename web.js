bcd472d3ffa82f9a7c906d24ee9b47704a66eab9
dvizcaya91 <dvizcaya91@gmail.com>
2013-07-10 16:29:33 +0000

    ReadFile Sync corrrected 4

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs=require("fs"), sys=require("sys");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fil=express.readFileSync('index.html');
+var fil=fs.readFileSync('index.html');
   response.send(fil);
 });
 
