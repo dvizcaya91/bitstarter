544cc24f13e004148ac17a47cbb6bef4d63d074d
dvizcaya91 <dvizcaya91@gmail.com>
2013-07-10 16:22:03 +0000

    ReadFile Sync corrrected 3

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fil=express.readFileSync(index.html);
+var fil=express.readFileSync('index.html');
   response.send(fil);
 });
 
